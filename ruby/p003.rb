4babd17e9baaf7864ba81470f8d59e4896fe8c2b
Daniel Whitston <dan@whitston.org.uk>
2020-09-26 08:43:02 +0100

    Tidy up p003 ruby description

--- ruby/p003.rb
+++ ruby/p003.rb
@@ -4,8 +4,10 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-# Simple solution: the lowest divisor that leaves 0 leftover is the lowest prime factor.
-# Dividing by that gives the largest prime factor?
+# Simple solution: manually iterate through every possible divisor from 2 upwards, and divide whenever
+# possible without remainder. When the divisor has the same value as the remainder, we have the prime factor
+
+# Find the largest prime factor of a number
 def p003(total_remaining = 600_851_475_143)
   divisor = 2
   until divisor >= total_remaining
